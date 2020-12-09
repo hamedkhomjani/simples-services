1f3b837d79fd66f059967e43d35b64074adb898b
Hamed Khomjani <64583620+hamedkhomjani@users.noreply.github.com>
2020-12-09 18:49:08 +0100

    adding pics and cods

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./App.css"
 
 function App() {
   return (
@@ -6,46 +7,24 @@ function App() {
 
       <section className="hero">
           <div className="navbar">
-              <img src="/assets/logoandicons/2trans.png" alt="logo" className="logo"/>
-        
+              <img src="/assets/Group1.svg" alt="Simple Services logo" className="logo"/>
+              <nav className="navlinks">
+                <a href="">Home</a>
+                <a href="">Services</a>
+                <a href="">About</a>
+                <a href="">Contact</a>
+              </nav>
           </div>
         
           <div className="content">
-              <div className="m">
                   <div className="m2">
-                      <h3>Welcome</h3>
-                      <h1>Discover Arts of Cars' world with CarArt.</h1>
-                      <button type="button">Take a tour</button>
+                      <h1>BEST “<span>IT</span>” SERVICES BUT SIMPLE</h1>
                   </div>
-                  <img className="do002" src="/assets/images/4 trans.png" alt=""/>
-              </div>
+              
           </div>
       </section>
-      <div className="side-bar">
-          <a><img src="/assets/logoandicons/Hamburger_icon.svg.png" alt="menulogo" className="menu"/></a>
-        
-          <nav className="social-links">
-              <a href="https://www.facebook.com/" className="socialLink" target="blank">
-                  <img src="/assets/logoandicons/facebook.svg" className="socialIcon" alt=""/>
-              </a>
-              <a href="https://www.instagram.com/" className="socialLink" target="blank">
-                  <img src="/assets/logoandicons/instagram.svg" className="socialIcon" alt=""/>
-              </a>
-              <a href="https://www.twitter.com/" className="socialLink" target="blank">
-                  <img src="/assets/logoandicons/twitter.svg" className="socialIcon" alt=""/>
-              </a>
-          </nav>
-      </div>
         
       <section className="works">
-          <div className="work1">
-              <img className="workpic" src="/assets/images/25.jpg" alt="Porsche painting"/>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra mauris laoreet blandit semper. Fusce fringilla mi nisi, eu dapibus elit vulputate ut. Vestibulum aliquam venenatis euismod. Integer quis urna nec est interdum maximus.</p>
-          </div>
-          <div className="work2">
-              <img className="workpic" S src="/assets/images/12.png" alt="Porsche painting"/>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra mauris laoreet blandit semper. Fusce fringilla mi nisi, eu dapibus elit vulputate ut. Vestibulum aliquam venenatis euismod. Integer quis urna nec est interdum maximus.</p>
-          </div>
         
       </section>
         
